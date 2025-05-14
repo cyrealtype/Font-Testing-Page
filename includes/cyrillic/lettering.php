0ebb4c69c397fcc33e086b92dcf89989e52d88e1
Alexei Vanyashin <a+mygit@cyreal.org>
2025-05-15 01:42:15 +0500

    improved cyrillic lists

--- includes/cyrillic/lettering.php
+++ includes/cyrillic/lettering.php
@@ -1,15 +1,33 @@
-<div contenteditable="true" style="width: 1020px; text-align: center; padding: 20px 20px;">
+<div contenteditable="true" style="width: 880px; text-align: center; padding: 20px 20px; font-feature-settings: 'locl';">
 	
 	<p class="sizelabel">Lettering Sheet 72 points</p>
 	
-	<p style="font-size: 72px;">А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я<br />
-	а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я<br />
+	<p style="font-size: 72px;">А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я Ґ Є І Ї Ђ Љ Њ Ћ<br />
+	а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я
+	ґ є і ї ђ љ њ ћ<br />
+	<p class="sizelabel">Bulgarian locl</p>
+	<span lang="bg">Ф Д Л в г д ж з и й к л п т ц ш щ ь ъ ю</span>
+	<p class="sizelabel">Serbian/Macedonian locl</p>
+	<span lang="sr">б г д п т</span><br />
 	1 2 3 4 5 6 7 8 9 0<br />
 	<br />
-	A B C D E F G H I J K L M N O P Q R S T U V W X Y Z<br />
-	a b c d e f g h i j k l m n o p q r s t u v w x y z<br />
-	<br />
 	( { [ . , ¡ ! ¿ ? * ' ‘ ’ " “ ” ] } )<br />
-	$ € £ % @ &amp; ¶ § ¢ † ‡ </p>
+	₽ ₴ ₸ &#x058F; № % @ &amp; ¶ § ¢ † ‡ </p>
+
+	<p style="font-size: 36px;">
+		АБВГҐЃДЂЕЁЄЖЗЗ́ЅИІЇЙЈКЛЉМНЊОПРСС́ТЋЌУЎФХЦЧЏШЩЪЫЬЭЮЯ<br />
+		абвгґѓдђеёєжзз́ѕиіїйјклљмнњопрсс́тћќуўфхцчџшщъыьэюя<br />
+	</p>
+
+	<p class="sizelabel">Bulgarian locl</p>
+	<p style="font-size: 72px;" lang="bg">
+		А Б В Г Д Е Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ь Ю Я<br />
+		а б в г д е ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ь ю я<br />
+	</p>
+	<p class="sizelabel">Serbian locl</p>
 	
+	<p style="font-size: 72px;" lang="sr">
+		АБВГДЂЕЖЗИЈКЛЉМНЊОПРСТЋУФХЦЧЏШ<br />
+		абвгдђежзијклљмнњопрстћуфхцчџш<br />
+	</p>
 </div>	
